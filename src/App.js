7b932b95146bf7ea13c0c72e8f81c67bd0843ef8
Akash-Traveller <akash.arkvk@gmail.com>
2022-08-24 16:48:11 +0530

    redux basics code - 24-8-22

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Container from "./components/Container";
+import { useDispatch , useSelector } from 'react-redux';
+import { calculateTotals , getCartItems } from './feature/cart/cartSlice';
+import { useEffect } from "react";
+import Modal from "./components/Modal";
 
 function App() {
+  const { cartItems , isLoading } = useSelector((store) => store.cart)
+  const { isOpen } = useSelector((store) => store.modal)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, []);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Redux
-        </a>
-      </header>
-    </div>
+  <main>
+    {isOpen && <Modal/>}
+    <Navbar/>
+    <Container/>
+  </main>
   );
 }
-
 export default App;
---
+++ src/components/Modal.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { closeModal } from '../feature/modal/modalSlice'
+import { clearCart } from '../feature/cart/cartSlice'
+import { useDispatch } from 'react-redux'
+
+const Modal = () => {
+    const dispatch = useDispatch();
+  return (
+    <aside className='modal-container'>
+        <div className='modal'>
+         <h4>remove all items from cart</h4>
+        <div className='btn-container'>
+            <button type='button' className='btn confirm-btn'
+            onClick={() => {
+                dispatch(clearCart())
+                dispatch(closeModal())
+            }}
+            >
+                Confirm
+            </button>
+            <button type='button' className='btn clear-btn'  onClick={() => {
+                dispatch(closeModal())
+            }}>
+                Cancel
+            </button>
+        </div> 
+        </div>
+    </aside>
+  )
+}
+
+export default Modal
